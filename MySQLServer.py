7ab0ac2022c4e42f559d30d5709aff9692f5e909
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-10-14 23:36:18 +0300

    add file

---
+++ MySQLServer.py
@@ -0,0 +1,22 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='your_username',
+        password='your_password'
+    )
+    
+    cursor = connection.cursor()
+    create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+    cursor.execute(create_db_query)
+    print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    cursor.close()
+    connection.close()
+    print("MySQL connection is closed.")
